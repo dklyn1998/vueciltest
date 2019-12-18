d15b86e88e563f839415ce42871d367a5f287180
段珂 <dk19980308@qq.com>
2019-12-18 13:44:53 +0800

    tabbar

---
+++ tabbar/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+<div>
+  <router-view></router-view>
+  <tab-bar>
+    <tab-bar-item path='/home' activeColor='blue'>
+      <img src="./assets/img/tabbar/logo.png" alt="" slot="item-icon">
+      <img src="./assets/img/tabbar/heima.b5a855ee.png" alt="" slot="item-icon-active">
+      <div slot='item-text'>首页</div>
+    </tab-bar-item>
+    <tab-bar-item path='/category' activeColor='blue'>
+      <img src="./assets/img/tabbar/logo.png" alt="" slot="item-icon">
+      <img src="./assets/img/tabbar/heima.b5a855ee.png" alt="" slot="item-icon-active">
+      <div slot='item-text'>分类</div>
+    </tab-bar-item>
+    <tab-bar-item path='/cart' activeColor='yellow'>
+      <img src="./assets/img/tabbar/logo.png" alt="" slot="item-icon">
+      <img src="./assets/img/tabbar/heima.b5a855ee.png" alt="" slot="item-icon-active">
+      <div slot='item-text'>购物车</div>
+    </tab-bar-item>
+    <tab-bar-item path='/profile' activeColor='red'>
+      <img src="./assets/img/tabbar/logo.png" alt="" slot="item-icon">
+      <img src="./assets/img/tabbar/heima.b5a855ee.png" alt="" slot="item-icon-active">
+      <div slot='item-text'>我的</div>
+    </tab-bar-item>
+  </tab-bar>
+  </div>
+</template>
+
+<script>
+import TabBar from "./components/tabbar/Tabbar";
+import TabBarItem from "./components/tabbar/TabBarItem";
+export default {
+  name: 'App',
+  components:{
+    TabBar,
+    TabBarItem
+  }
+}
+</script>
+
+<style>
+@import './assets/css/base.css';
+
+</style>
---
+++ tabbar/src/components/tabbar/TabBarItem.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="tab-bat-item" @click="itemClick">
+      <div  v-if="!isActive"><slot name='item-icon'></slot></div>
+      <div v-else><slot name='item-icon-active' ></slot></div>
+      <div :style="activeStyle"><slot name='item-text'></slot></div>
+     
+  </div>
+</template>
+
+<script>
+export default {
+    props:{
+        path:String,
+        activeColor:{
+            type:String,
+            default:'red'
+        }
+    },
+    data(){
+        return{
+            // isActive:true
+        }
+    },
+    computed:{
+        isActive(){
+            return this.$route.path.indexOf(this.path) !== -1
+        },
+        activeStyle(){
+            return this.isActive?{color:this.activeColor}:{}
+        }
+    },
+    methods:{
+        itemClick(){
+            // console.log('itemClick');
+            this.$router.push(this.path)
+            
+        }
+    }
+}
+</script>
+
+<style>
+.tab-bat-item{
+ flex: 1;
+ text-align: center;
+ height: 49px;
+ font-size: 14px;
+}
+.tab-bat-item img{
+    width: 24px;
+    height: 24px;
+    margin-top: 3px;
+    vertical-align: middle;
+    margin-bottom: 2px;
+}
+.active{
+    color: red;
+}
+</style>
